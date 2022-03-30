e5993144720a82549ac25b11a3665ed132edb7b3
Bintaaaa <bijantium.yt.32@gmail.com>
2022-03-30 21:49:59 +0700

    data dummy

--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -1,7 +1,9 @@
 import 'package:ditonton/data/models/movie_table.dart';
+import 'package:ditonton/data/models/tv_show_model.dart';
 import 'package:ditonton/domain/entities/genre.dart';
 import 'package:ditonton/domain/entities/movie.dart';
 import 'package:ditonton/domain/entities/movie_detail.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
 
 final testMovie = Movie(
   adult: false,
@@ -57,3 +59,28 @@ final testMovieMap = {
   'posterPath': 'posterPath',
   'title': 'title',
 };
+
+//tv
+
+final testTVShowModel = TVShowModel(
+  backdropPath: "/oC9SgtJTDCEpWnTBtVGoAvjl5hb.jpg",
+  firstAirDate: "2006-09-18",
+  genreIds: [10767],
+  id: 1991,
+  name: "Rachael Ray",
+  originCountry: ["US"],
+  originalLanguage: "en",
+  originalName: "Rachael Ray",
+  overview:
+      "Rachael Ray, also known as The Rachael Ray Show, is an American talk show starring Rachael Ray that debuted in syndication in the United States and Canada on September 18, 2006. It is filmed at Chelsea Television Studios in New York City. The show's 8th season premiered on September 9, 2013, and became the last Harpo show in syndication to switch to HD with a revamped studio. In January 2012, CBS Television Distribution announced a two-year renewal for the show, taking it through the 2013–14 season.",
+  popularity: 1765.863,
+  posterPath: "/dsAJhCLYX1fiNRoiiJqR6Up4aJ.jpg",
+  voteAverage: 5.8,
+  voteCount: 29,
+);
+
+final testTVShowModelList = <TVShowModel>[testTVShowModel];
+
+final testTVShow = testTVShowModel.toEntity();
+
+final testTVShowList = <TVShow>[testTVShow];
