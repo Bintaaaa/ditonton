486e0ffcb77c325ff777a3a795c3903d865b39fd
Bintaaaa <bijantium.yt.32@gmail.com>
2022-03-31 00:46:33 +0700

    create page tv show and showing now playing tv

---
+++ lib/presentation/pages/home_tv_page.dart
@@ -0,0 +1,105 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/presentation/provider/tv_show_list_notifier.dart';
+import 'package:ditonton/presentation/widgets/drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeTVPage extends StatefulWidget {
+  static const ROUTE_NAME = '/home-tv';
+  const HomeTVPage({Key? key}) : super(key: key);
+
+  @override
+  State<HomeTVPage> createState() => _HomeTVPageState();
+}
+
+class _HomeTVPageState extends State<HomeTVPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => Provider.of<TVShowListNotifier>(context, listen: false)
+        ..fetchNowPlayingTVShows(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: DrawerDitonton(),
+      appBar: AppBar(
+        title: Text('Ditonton'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Now Playing",
+                style: kHeading6,
+              ),
+              Consumer<TVShowListNotifier>(builder: (context, data, child) {
+                final state = data.nowPlayingState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TVList(data.nowPlayingTVShows);
+                } else {
+                  return Text('Failed to fetch data');
+                }
+              })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TVList extends StatelessWidget {
+  final List<TVShow> tvShows;
+
+  TVList(this.tvShows);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final tvShow = tvShows[index];
+          return Container(
+            padding: const EdgeInsets.all(8),
+            child: InkWell(
+              onTap: () {},
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                child: CachedNetworkImage(
+                  imageUrl: '$BASE_IMAGE_URL${tvShow.posterPath}',
+                  placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                ),
+              ),
+            ),
+          );
+        },
+        itemCount: tvShows.length,
+      ),
+    );
+  }
+}
