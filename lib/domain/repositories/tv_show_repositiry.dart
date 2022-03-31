12d4637c8ad34b9feb09c70aa599eefa9732d04e
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-01 02:08:44 +0700

    add abstrac get tv show detail

--- lib/domain/repositories/tv_show_repositiry.dart
+++ lib/domain/repositories/tv_show_repositiry.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/entities/tv_show_detail.dart';
 
 abstract class TVShowRepository {
   Future<Either<Failure, List<TVShow>>> getNowPlayingTVShows();
+  Future<Either<Failure, TVShowDetail>> getTVShowDetail(int id);
 }
