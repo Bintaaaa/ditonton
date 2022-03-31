9e6292837cb6b9b85920587494a6b8def5c0bc3f
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-01 02:07:53 +0700

    create usecase get tv show detail

---
+++ lib/domain/usecases/get_tv_show_detail.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entities/tv_show_detail.dart';
+import 'package:ditonton/domain/repositories/tv_show_repositiry.dart';
+
+class GetTVShowDetail {
+  final TVShowRepository repository;
+  GetTVShowDetail(this.repository);
+
+  Future<Either<Failure, TVShowDetail>> execute(int id) {
+    return repository.getTVShowDetail(id);
+  }
+}
