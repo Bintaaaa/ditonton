e85fdbecf9f21cbc92ecb7dec8c3f352b5b06498
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-02 02:12:38 +0700

    testing get tv show recommedations

--- test/data/repositories/tv_show_repository_impl_test.dart
+++ test/data/repositories/tv_show_repository_impl_test.dart
@@ -106,4 +106,49 @@ void main() {
           equals(Left(ConnectionFailure('Failed to connect to the network'))));
     });
   });
+
+  group('Get TV Show Recommendations', () {
+    final tId = 1;
+
+    test('should return data (tv show list) when the call is successful',
+        () async {
+      // arrange
+      when(mockTVShowRemoteDataSource.getTVShowRecommendations(tId))
+          .thenAnswer((_) async => testTVShowModelList);
+      // act
+      final result = await repositoryImpl.getTVShowRecommendations(tId);
+      // assert
+      verify(mockTVShowRemoteDataSource.getTVShowRecommendations(tId));
+      /* workaround to test List in Right. Issue: https://github.com/spebbe/dartz/issues/80 */
+      final resultList = result.getOrElse(() => []);
+      expect(resultList, equals(testTVShowList));
+    });
+
+    test(
+        'should return server failure when call to remote data source is unsuccessful',
+        () async {
+      // arrange
+      when(mockTVShowRemoteDataSource.getTVShowRecommendations(tId))
+          .thenThrow(ServerException());
+      // act
+      final result = await repositoryImpl.getTVShowRecommendations(tId);
+      // assertbuild runner
+      verify(mockTVShowRemoteDataSource.getTVShowRecommendations(tId));
+      expect(result, equals(Left(ServerFailure(''))));
+    });
+
+    test(
+        'should return connection failure when the device is not connected to the internet',
+        () async {
+      // arrange
+      when(mockTVShowRemoteDataSource.getTVShowRecommendations(tId))
+          .thenThrow(SocketException('Failed to connect to the network'));
+      // act
+      final result = await repositoryImpl.getTVShowRecommendations(tId);
+      // assert
+      verify(mockTVShowRemoteDataSource.getTVShowRecommendations(tId));
+      expect(result,
+          equals(Left(ConnectionFailure('Failed to connect to the network'))));
+    });
+  });
 }
