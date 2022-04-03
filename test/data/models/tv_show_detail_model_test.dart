8ec6d11222f08eec08016bf45c3a0c652a2060aa
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-03 16:14:53 +0700

    resvision testing data model

--- test/data/models/tv_show_detail_model_test.dart
+++ test/data/models/tv_show_detail_model_test.dart
@@ -4,19 +4,46 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../dummy_data/dummy_objects.dart';
 
 void main() {
-  group('TV Show Detail Model', () {
-    test('should be a subclass of TVShowDetail entity', () {
-      final result = testTVShowDetail.toEntity();
-      expect(result, testTVShowDetailEntity);
+  final testTVShowResponse = TVShowDetailModel(
+    popularity: 1,
+    posterPath: 'posterPath',
+    name: 'name',
+    type: 'type',
+    numberOfEpisodes: 1,
+    numberOfSeasons: 1,
+    seasons: [],
+    episodeRunTime: [1],
+    firstAirDate: 'firstAirDate',
+    genres: [],
+    id: 1,
+    overview: 'overview',
+    voteCount: 1,
+    tagline: 'tagline',
+    originalName: 'originalName',
+    homepage: 'homepage',
+    voteAverage: 1,
+    originalLanguage: 'originalLanguage',
+    backdropPath: 'backdropPath',
+    status: 'status',
+  );
+
+  final testTVShowDetail = testTVShowResponse.toEntity();
+  final testTVShowMap = testTVShowResponse.toJson();
+
+  group('Model of TV Show Detail', () {
+    test('should be a subclass of TVShowDetail entity', () async {
+      final result = testTVShowResponse.toEntity();
+      expect(result, testTVShowDetail);
     });
+
     test('should be a map of TVShow', () async {
-      final result = testTVShowDetail.toJson();
+      final result = testTVShowResponse.toJson();
       expect(result, testTVShowMap);
     });
 
     test('should be a TVShowDetailResponse instance', () async {
       final result = TVShowDetailModel.fromJson(testTVShowMap);
-      expect(result, testTVShowDetail);
+      expect(result, testTVShowResponse);
     });
   });
 }
