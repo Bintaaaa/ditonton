2afd2092ef76f59d68e5a9b279f22f86e421178d
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-03 16:02:14 +0700

    testinig usecase popular tv shows

---
+++ test/domain/usecases/get_popular_tv_show_test.dart
@@ -0,0 +1,34 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/data/datasources/tv_remote_data_source.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_shows.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetPopularTVShows usecase;
+  late MockTVShowRepository repository;
+
+  setUp(() {
+    repository = MockTVShowRepository();
+    usecase = GetPopularTVShows(repository);
+  });
+
+  final testTVShow = <TVShow>[];
+
+  group('Get Popular TVShows', () {
+    test(
+        'should get list of tv shows form the reposiroty exeecute function is called',
+        () async {
+      //arrange
+      when(repository.getPopularTVShows())
+          .thenAnswer((_) async => Right(testTVShow));
+      //act
+      final result = await usecase.execute();
+      //assert
+      expect(result, Right(testTVShow));
+    });
+  });
+}
