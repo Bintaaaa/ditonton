42aa654447f9da63bbbabe5fef629a5a2fb0e2f7
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-03 16:14:15 +0700

    after testing presentation

--- lib/presentation/pages/home_tv_page.dart
+++ lib/presentation/pages/home_tv_page.dart
@@ -3,6 +3,7 @@ import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/domain/entities/tv_show.dart';
 import 'package:ditonton/presentation/pages/tv_show_detail_page.dart';
+import 'package:ditonton/presentation/provider/popular_tv_shows_notifier.dart';
 import 'package:ditonton/presentation/provider/tv_show_list_notifier.dart';
 import 'package:ditonton/presentation/widgets/drawer.dart';
 import 'package:flutter/material.dart';
@@ -22,7 +23,8 @@ class _HomeTVPageState extends State<HomeTVPage> {
     super.initState();
     Future.microtask(
       () => Provider.of<TVShowListNotifier>(context, listen: false)
-        ..fetchNowPlayingTVShows(),
+        ..fetchNowPlayingTVShows()
+        ..fetchPopularTVShows(),
     );
   }
 
@@ -60,13 +62,47 @@ class _HomeTVPageState extends State<HomeTVPage> {
                 } else {
                   return Text('Failed to fetch data');
                 }
-              })
+              }),
+              _buildSubHeading(title: 'Popular', onTap: () {}),
+              Consumer<TVShowListNotifier>(builder: (context, data, child) {
+                final state = data.popularTVShowsState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TVList(data.popularTVShows);
+                } else {
+                  return Text('Failed');
+                }
+              }),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+Row _buildSubHeading({required String title, required Function() onTap}) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Text(
+        title,
+        style: kHeading6,
+      ),
+      InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [Text('See More'), Icon(Icons.arrow_forward_ios)],
+          ),
+        ),
+      ),
+    ],
+  );
 }
 
 class TVList extends StatelessWidget {
--- lib/presentation/provider/tv_show_list_notifier.dart
+++ lib/presentation/provider/tv_show_list_notifier.dart
@@ -1,21 +1,30 @@
 import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/domain/entities/tv_show.dart';
 import 'package:ditonton/domain/usecases/get_now_playing_tv_shows.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_shows.dart';
 import 'package:flutter/cupertino.dart';
 
 class TVShowListNotifier extends ChangeNotifier {
   var _nowPlayingTVShows = <TVShow>[];
   List<TVShow> get nowPlayingTVShows => _nowPlayingTVShows;
 
+  var _popularTVShows = <TVShow>[];
+  List<TVShow> get popularTVShows => _popularTVShows;
+
   RequestState _nowPlayingState = RequestState.Empty;
   RequestState get nowPlayingState => _nowPlayingState;
+
+  RequestState _popularTVShowsState = RequestState.Empty;
+  RequestState get popularTVShowsState => _popularTVShowsState;
 
   String _message = '';
   String get message => _message;
 
   final GetNowPlayingTVShows getNowPlayingTVShows;
+  final GetPopularTVShows getPopularTVShows;
   TVShowListNotifier({
     required this.getNowPlayingTVShows,
+    required this.getPopularTVShows,
   });
 
   Future<void> fetchNowPlayingTVShows() async {
@@ -32,5 +41,24 @@ class TVShowListNotifier extends ChangeNotifier {
       _nowPlayingTVShows = tvShows;
       notifyListeners();
     });
+  }
+
+  Future<void> fetchPopularTVShows() async {
+    _popularTVShowsState = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getPopularTVShows.execute();
+    result.fold(
+      (failure) {
+        _popularTVShowsState = RequestState.Error;
+        _message = failure.message;
+        notifyListeners();
+      },
+      (tvShowsData) {
+        _popularTVShowsState = RequestState.Loaded;
+        _popularTVShows = tvShowsData;
+        notifyListeners();
+      },
+    );
   }
 }
