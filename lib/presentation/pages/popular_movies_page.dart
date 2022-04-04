b4fc22f957a65fc7f634cbd29d8ff1ef29dde758
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-04 23:18:43 +0700

    popular tv shows

--- lib/presentation/pages/popular_movies_page.dart
+++ lib/presentation/pages/popular_movies_page.dart
@@ -1,6 +1,6 @@
 import 'package:ditonton/common/state_enum.dart';
 import 'package:ditonton/presentation/provider/popular_movies_notifier.dart';
-import 'package:ditonton/presentation/widgets/movie_card_list.dart';
+import 'package:ditonton/presentation/widgets/card_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -38,7 +38,9 @@ class _PopularMoviesPageState extends State<PopularMoviesPage> {
               return ListView.builder(
                 itemBuilder: (context, index) {
                   final movie = data.movies[index];
-                  return MovieCard(movie);
+                  return CardList(
+                    movie: movie,
+                  );
                 },
                 itemCount: data.movies.length,
               );
