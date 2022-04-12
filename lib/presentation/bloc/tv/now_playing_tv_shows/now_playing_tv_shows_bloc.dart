ccb30866e59ac05154ab6dc81c52e113e0fe382d
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-13 01:01:26 +0700

    statemanagement now playing tv show with bloc non module

---
+++ lib/presentation/bloc/tv/now_playing_tv_shows/now_playing_tv_shows_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_now_playing_tv_shows.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'now_playing_tv_shows_event.dart';
+part 'now_playing_tv_shows_state.dart';
+
+class NowPlayingTVShowsBloc
+    extends Bloc<NowPlayingTVShowsEvent, NowPlayingTVShowsState> {
+  final GetNowPlayingTVShows _getNowPlayingTVShows;
+
+  NowPlayingTVShowsBloc(
+    this._getNowPlayingTVShows,
+  ) : super(NowPlayingTVShowsEmpty()) {
+    on<OnNowPlayingTVShowsCalled>(_onNowPlayingTVShowsCalled);
+  }
+
+  FutureOr<void> _onNowPlayingTVShowsCalled(NowPlayingTVShowsEvent event,
+      Emitter<NowPlayingTVShowsState> emit) async {
+    emit(NowPlayingTVShowsLoading());
+
+    final result = await _getNowPlayingTVShows.execute();
+
+    result.fold(
+      (failure) {
+        emit(NowPlayingTVShowsError(failure.message));
+      },
+      (data) {
+        data.isEmpty
+            ? emit(NowPlayingTVShowsEmpty())
+            : emit(NowPlayingTVShowsHasData(data));
+      },
+    );
+  }
+}
