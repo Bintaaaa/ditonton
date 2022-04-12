cedf2a6a069ef2d48699a59038ebcbea5c72f479
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-13 01:04:24 +0700

    state management detail tv show with bloc non module

---
+++ lib/presentation/bloc/tv/tv_show_detail/tv_show_detail_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:ditonton/domain/entities/tv_show_detail.dart';
+import 'package:ditonton/domain/usecases/get_tv_show_detail.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'tv_show_detail_event.dart';
+part 'tv_show_detail_state.dart';
+
+class TVShowDetailBloc extends Bloc<TVShowDetailEvent, TVShowDetailState> {
+  final GetTVShowDetail _getTVShowDetail;
+
+  TVShowDetailBloc(this._getTVShowDetail) : super(TVShowDetailEmpty()) {
+    on<OnTVShowDetailCalled>(_onTVShowDetailCalled);
+  }
+
+  FutureOr<void> _onTVShowDetailCalled(
+    OnTVShowDetailCalled event,
+    Emitter<TVShowDetailState> emit,
+  ) async {
+    final id = event.id;
+
+    emit(TVShowDetailLoading());
+
+    final result = await _getTVShowDetail.execute(id);
+
+    result.fold(
+      (failure) {
+        emit(TVShowDetailError(failure.message));
+      },
+      (data) {
+        emit(TVShowDetailHasData(data));
+      },
+    );
+  }
+}
