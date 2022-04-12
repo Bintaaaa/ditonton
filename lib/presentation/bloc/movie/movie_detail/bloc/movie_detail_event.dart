43ced8e9e73ba5d7b2ec224420b60cc8c930c1b1
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-12 23:44:00 +0700

    state mangement with bloc non modular

---
+++ lib/presentation/bloc/movie/movie_detail/bloc/movie_detail_event.dart
@@ -0,0 +1,12 @@
+part of 'movie_detail_bloc.dart';
+
+abstract class MovieDetailEvent extends Equatable {}
+
+class OnMovieDetailCalled extends MovieDetailEvent {
+  final int id;
+
+  OnMovieDetailCalled(this.id);
+
+  @override
+  List<Object?> get props => [];
+}
