08ebaefb4c89e1e55cce31bb3b12739094d3d941
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-13 01:02:18 +0700

    state management popular tv show with bloc non module

---
+++ lib/presentation/bloc/tv/popular_tv_shows/popular_tv_shows_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_shows.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'popular_tv_shows_event.dart';
+part 'popular_tv_shows_state.dart';
+
+class PopularTVShowsBloc
+    extends Bloc<PopularTVShowsEvent, PopularTVShowsState> {
+  final GetPopularTVShows _getPopularTVShows;
+
+  PopularTVShowsBloc(this._getPopularTVShows) : super(PopularTVShowsEmpty()) {
+    on<OnPopularTVShowsCalled>(_onPopularTVShowsCalled);
+  }
+
+  FutureOr<void> _onPopularTVShowsCalled(
+    OnPopularTVShowsCalled event,
+    Emitter<PopularTVShowsState> emit,
+  ) async {
+    emit(PopularTVShowsLoading());
+
+    final result = await _getPopularTVShows.execute();
+
+    result.fold(
+      (failure) {
+        emit(PopularTVShowsError(failure.message));
+      },
+      (data) {
+        data.isEmpty
+            ? emit(PopularTVShowsEmpty())
+            : emit(PopularTVShowsHasData(data));
+      },
+    );
+  }
+}
