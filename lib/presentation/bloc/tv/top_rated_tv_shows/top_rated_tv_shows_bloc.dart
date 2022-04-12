b7fbf0b307c5ba0ffbcffec390fd3db7298993f6
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-13 01:03:25 +0700

    state management top rated tv show with bloc non module

---
+++ lib/presentation/bloc/tv/top_rated_tv_shows/top_rated_tv_shows_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:ditonton/domain/entities/tv_show.dart';
+import 'package:ditonton/domain/usecases/get_top_rated_tv_shows.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'top_rated_tv_shows_event.dart';
+part 'top_rated_tv_shows_state.dart';
+
+class TopRatedTVShowsBloc
+    extends Bloc<TopRatedTVShowsEvent, TopRatedTVShowsState> {
+  final GetTopRatedTVShows _getTopRatedTVShows;
+
+  TopRatedTVShowsBloc(this._getTopRatedTVShows)
+      : super(TopRatedTVShowsEmpty()) {
+    on<OnTopRatedTVShowsCalled>(_onTopRatedTVShowsCalled);
+  }
+
+  FutureOr<void> _onTopRatedTVShowsCalled(
+    OnTopRatedTVShowsCalled event,
+    Emitter<TopRatedTVShowsState> emit,
+  ) async {
+    emit(TopRatedTVShowsLoading());
+
+    final result = await _getTopRatedTVShows.execute();
+
+    result.fold(
+      (failure) {
+        emit(TopRatedTVShowsError(failure.message));
+      },
+      (data) {
+        data.isEmpty
+            ? emit(TopRatedTVShowsEmpty())
+            : emit(TopRatedTVShowsHasData(data));
+      },
+    );
+  }
+}
