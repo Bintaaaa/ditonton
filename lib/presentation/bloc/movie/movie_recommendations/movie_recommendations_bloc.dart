3378ada4f516ece7fed6f05572ed28d5c05d15cd
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-12 23:47:38 +0700

    state management movie recomendation with bloc non modular

---
+++ lib/presentation/bloc/movie/movie_recommendations/movie_recommendations_bloc.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_movie_recommendations.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'movie_recommendations_event.dart';
+part 'movie_recommendations_state.dart';
+
+class MovieRecommendationsBloc
+    extends Bloc<MovieRecommendationsEvent, MovieRecommendationsState> {
+  final GetMovieRecommendations _getMovieRecommendations;
+
+  MovieRecommendationsBloc(this._getMovieRecommendations)
+      : super(MovieRecommendationsEmpty()) {
+    on<OnMovieRecommendationsCalled>(_onMovieRecommendationsCalled);
+  }
+
+  FutureOr<void> _onMovieRecommendationsCalled(
+    OnMovieRecommendationsCalled event,
+    Emitter<MovieRecommendationsState> emit,
+  ) async {
+    final id = event.id;
+    emit(MovieRecommendationsLoading());
+
+    final result = await _getMovieRecommendations.execute(id);
+
+    result.fold(
+      (failure) {
+        emit(MovieRecommendationsError(failure.message));
+      },
+      (data) {
+        data.isEmpty
+            ? emit(MovieRecommendationsEmpty())
+            : emit(MovieRecommendationsHasData(data));
+      },
+    );
+  }
+}
