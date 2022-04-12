a8287dc651bd36a4703c965a1fc7a349e23b7d16
Bintaaaa <bijantium.yt.32@gmail.com>
2022-04-12 23:44:52 +0700

    state management with bloc non modular

---
+++ lib/presentation/bloc/movie/popular_movies/bloc/popular_movies_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/usecases/get_popular_movies.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+part 'popular_movies_event.dart';
+part 'popular_movies_state.dart';
+
+class PopularMoviesBloc extends Bloc<PopularMoviesEvent, PopularMoviesState> {
+  final GetPopularMovies _getPopularMovies;
+
+  PopularMoviesBloc(this._getPopularMovies) : super(PopularMoviesEmpty()) {
+    on<OnPopularMoviesCalled>(_onPopularMoviesCalled);
+  }
+
+  FutureOr<void> _onPopularMoviesCalled(
+    OnPopularMoviesCalled event,
+    Emitter<PopularMoviesState> emit,
+  ) async {
+    emit(PopularMoviesLoading());
+
+    final result = await _getPopularMovies.execute();
+
+    result.fold(
+      (failure) {
+        emit(PopularMoviesError(failure.message));
+      },
+      (data) {
+        data.isEmpty
+            ? emit(PopularMoviesEmpty())
+            : emit(PopularMoviesHasData(data));
+      },
+    );
+  }
+}
